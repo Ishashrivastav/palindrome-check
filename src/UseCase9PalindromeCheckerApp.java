942f906f418a2e9c9d6385d2a63e51a93769989c
Ishashrivastav <ishashrivastav06@gmail.com>
2026-02-25 18:57:31 +0530

    UC9:palindrome check

---
+++ src/UseCase9PalindromeCheckerApp.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class UseCase9PalindromeCheckerApp {
+
+    // Recursive function to check palindrome
+    public static boolean isPalindrome(String str, int start, int end) {
+
+        // Base Condition 1: If pointers cross or meet
+        if (start >= end) {
+            return true;
+        }
+
+        // If characters do not match
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
+        }
+
+        // Recursive call for inner substring
+        return isPalindrome(str, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("===== Recursive Palindrome Checker =====");
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        boolean result = isPalindrome(input, 0, input.length() - 1);
+
+        if (result) {
+            System.out.println("Result: The given string is a PALINDROME.");
+        } else {
+            System.out.println("Result: The given string is NOT a palindrome.");
+        }
+
+        scanner.close();
+    }
+}
